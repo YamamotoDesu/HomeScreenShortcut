db1bf9c32eda5ba8474080fcfee2d6ff61e92ef9
yamamoto <kyoyamamoto1202@gmail.com>
2021-10-10 15:10:56 +0900

    Initial Commit

---
+++ HomeScreenShortcut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeScreenShortcut
+//
+//  Created by 山本響 on 2021/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
